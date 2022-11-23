7352c850ef2206ce686b3a2f1e74d27c283fbaab
Ignacio Diaz <mirete.casem@gmail.com>
2022-11-23 21:04:00 +0100

    punto 14

--- app/src/main/java/net/sessochoa/ignaciodiazmirete/practica3/MainActivity.kt
+++ app/src/main/java/net/sessochoa/ignaciodiazmirete/practica3/MainActivity.kt
@@ -2,10 +2,22 @@ package net.sessochoa.ignaciodiazmirete.practica3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import net.sessochoa.ignaciodiazmirete.practica3.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    var num:Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.tvNumero.text=num.toString()
+        binding.btSuma.setOnClickListener(){
+            num++
+            binding.tvNumero.text=num.toString()
+        }
     }
 }
