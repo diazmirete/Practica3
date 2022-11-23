5a04c2c01eebd2dce2aa7120b825f24c57b94889
Ignacio Diaz <mirete.casem@gmail.com>
2022-11-23 21:06:29 +0100

    punto 18

--- app/src/main/java/net/sessochoa/ignaciodiazmirete/practica3/MainActivity.kt
+++ app/src/main/java/net/sessochoa/ignaciodiazmirete/practica3/MainActivity.kt
@@ -2,13 +2,41 @@ package net.sessochoa.ignaciodiazmirete.practica3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import net.sessochoa.ignaciodiazmirete.practica3.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     var num:Int = 0
+    private const val TAG = "mostrar log"
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        Log.i(TAG, "onCreate")
+        override fun onStart() {
+            super.onStart()
+            Log.i(TAG, "onStart")
+        }
+        override fun onResume() {
+            super.onResume()
+            Log.i(TAG, "onResume")
+        }
+        override fun onPause() {
+            Log.i(TAG, "onPause")
+            super.onPause()
+        }
+        override fun onStop() {
+            Log.i(TAG, "onStop")
+            super.onStop()
+        }
+        override fun onRestart() {
+            super.onRestart()
+            Log.i(TAG, "onRestart")
+        }
+        override fun onDestroy() {
+            Log.i(TAG, "onDestroy")
+            super.onDestroy()
+        }
+
         super.onCreate(savedInstanceState)
         //setContentView(R.layout.activity_main)
         binding = ActivityMainBinding.inflate(layoutInflater)
